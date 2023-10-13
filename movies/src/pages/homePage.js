6c5fe21793daaab4c903dbedaf54f91270182cf1
Yuanzhe Yang <vinceHello@outlook.com>
2023-10-13 16:26:55 +0100

    Add to favourites logic completed; Dummy favourites page.

--- movies/src/pages/homePage.js
+++ movies/src/pages/homePage.js
@@ -6,6 +6,15 @@ import Grid from "@mui/material/Grid";
 
 
 const MovieListPage = (props) => {
+
+    const addToFavorites = (movieId) => {
+        const updatedMovies = movies.map((m) =>
+            m.id === movieId ? { ...m, favorite: true } : m
+        );
+        setMovies(updatedMovies);
+    };
+
+
     const [movies, setMovies] = useState([]);
 
     const [nameFilter, setNameFilter] = useState("");
@@ -53,7 +62,7 @@ const MovieListPage = (props) => {
                         genreFilter={genreFilter}
                     />
                 </Grid>
-                <MovieList movies={displayedMovies} />
+                <MovieList movies={displayedMovies} selectFavorite={addToFavorites} />
             </Grid>
         </Grid>
     );
